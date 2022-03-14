fc48c84e801514f4c63264cd9bea908f8c543737
Brad Moore <brad@4pi.com.au>
2022-03-14 18:49:45 +1100

    Updated to dotnet6

--- OWCE_Web/Pages/Index.cshtml.cs
+++ OWCE_Web/Pages/Index.cshtml.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace OWCE_Web.Pages
+namespace OWCE_Web.Pages;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    public void OnGet()
     {
-        public void OnGet()
-        {
 
-        }
     }
 }
